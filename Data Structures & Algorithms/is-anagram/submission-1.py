cb07f6eb945a7a8682373ab599be7d6e4d583834
Josephine-Leo <e1355234@u.nus.edu>
2026-09-06 00:57:08 +0800

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        # By using .get(key, fallback num), you save a
+        # if else condition check coz u take the curr count
+        # +1 or if dont exist yet is just fallback to 0
+        
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
